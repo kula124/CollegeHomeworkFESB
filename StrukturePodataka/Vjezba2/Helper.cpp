5ee6eb0370f1e7b48cf81f204a8cfec1c809bd59
Kula <kula124@gmail.com>
2016-10-19 12:57:51 +0200

    Vjezba2 push 2 (not done yet)

--- StrukturePodataka/Vjezba2/Helper.cpp
+++ StrukturePodataka/Vjezba2/Helper.cpp
@@ -1,29 +1,36 @@
 #include "Helper.h"
+optr CreateElement(char * n, char * l, int god)
+{
+	optr p = (optr)malloc(sizeof(_osoba));
+	if (!p)
+		return p;
+	if ((n == NULL || strlen(n) <= 2) && (l == NULL || strlen(l) <= 2))
+	{
+		p->name = NULL;
+		p->lname = NULL;
+		p->next = NULL;
+		p->born = 0;
+		return p;
+	}
+	p->name = (char*)malloc(sizeof(char) * strlen(n) + 1);
+	p->lname = (char*)malloc(sizeof(char) * strlen(l) + 1);
+	if (!p->name || !p->lname)
+		return NULL;
+	strcpy(p->name, n);
+	strcpy(p->lname, l);
+	p->born = god;
+	return p;
+}
+//--------------------------------------
+
 int AddToBegining(optr root, char * name, char * lname, int god)
 {
 	if (!root)
 		return ERROR_NULL_ROOT;
-	if (!root->next)
-	{
-		root->next = (optr)malloc(sizeof(_osoba));
-		if (!root->next)
-			return ERROR_ALLOCATING_MEMORY;
-		root->next->name = (char*)malloc(sizeof(char) * strlen(name) + 1);
-		root->next->lname = (char*)malloc(sizeof(char) * strlen(lname) + 1);
-		root->next->born = god;
-		if (!root->next->name || root->next->lname)
-			return ERROR_ALLOCATING_MEMORY;
-		strcpy(root->next->name, name);
-		strcpy(root->next->lname, lname);
-		root->next = NULL;
-	}
-	optr p = (optr)malloc(sizeof(_osoba));
+	optr p = CreateElement(name, lname, god);
 	if (!p)
 		return ERROR_ALLOCATING_MEMORY;
 	p->next = root->next;
-	strcpy(p->name, name);
-	strcpy(p->lname, lname);
-	p->born = god;
 	root->next = p;
 	return OK;
 }
@@ -31,39 +38,15 @@ int AddToBegining(optr root, char * name, char * lname, int god)
 
 int AddToEnd(optr root, char* name, char* lname, int god)
 {
-	if (!root->next)
-	{
-		root->next = (optr)malloc(sizeof(_osoba));
-		if (!root)
-			return ERROR_ALLOCATING_MEMORY;
-		puts("\nUnesite ime: ");
-		scanf("  %s", &name);
-		puts("Unesite prezime: ");
-		scanf("  %s", &lname);
-		puts("Unesite godinu rodjenja: ");
-		scanf("  %d", &god);
-		root->next->name = (char*)malloc(sizeof(char) * strlen(name) + 1);
-		root->next->lname = (char*)malloc(sizeof(char) * strlen(lname) + 1);
-		root->next->born = god;
-		if (!root->next->name || root->next->lname)
-			return ERROR_ALLOCATING_MEMORY;
-		strcpy(root->next->name, name);
-		strcpy(root->next->lname, lname);
-		root->next->next = NULL;
-		return OK;
-	}
+	if (!root)
+		return ERROR_NULL_ROOT;
 	optr p = root;
-	while (!p->next)
+	while (p->next)
 	{
 		p = p->next;
 	}
-	p->name = (char*)malloc(sizeof(char) * strlen(name) + 1);
-	p->lname = (char*)malloc(sizeof(char) * strlen(lname) + 1);
-	if (!p->name || p->lname)
-		return ERROR_ALLOCATING_MEMORY;
-	strcpy(p->name, name);
-	strcpy(p->lname, lname);
-	p->next = NULL;
+	p->next = CreateElement(name, lname, god);
+	p->next->next = NULL;
 	return OK;
 }
 //-------------------------------------------
@@ -73,7 +56,7 @@ int ShowList(optr root)
 	int i = 1;
 	optr p = root->next;
 	printf("Indx\tIme\tPrezime\t\tGod\n");
-	while (!p)
+	while (p)
 	{
 		printf("[%d] %s\t%s\t\t%d", i++, p->name, p->lname, p->born);
 		p = p->next;
@@ -84,9 +67,18 @@ int ShowList(optr root)
 }
 //--------------------------------------------
 
+int ShowElement(optr p)
+{
+	if (!p)
+		return ERROR_EMPTY_LIST;
+	printf("%s\t%s\t\t%d\n", p->name, p->lname, p->born);
+	return OK;
+}
+//--------------------------------------------
+
 int Delete(optr root, optr element)
 {
-	if (!element)
+	if (!element || !root)
 		return ERROR_UNESPECTED_NULL_PARAMETER;
 	if (!root->next)
 		return ERROR_EMPTY_LIST;
@@ -102,40 +94,45 @@ int Delete(optr root, optr element)
 }
 //---------------------------------------------
 
-int FindElements(optr root, optr** found, char* lastName, bool FindAll)
+int FindElements(optr root, optr** found, char* lastName, int* FindAll)
 {
-	int count = 0;
-	optr p = root;
+	int count = 1;
+	optr p = root->next;
 	*found = NULL;
-	if (FindAll > 0)
+	if (*FindAll > 0)
 	{
-		while (!p)
+		while (p)
 		{
-			if (strcmp(p->lname,lastName))
+			if (strcmp(p->lname,lastName) == 0)
 			{
-				*found = (optr*)realloc(*found, sizeof(optr) * count);
+				*found = (optr*)realloc(*found, sizeof(optr*) * count);
 				if (*found == NULL)
 					return ERROR_ALLOCATING_MEMORY;
-				*found[count] = p;
+				*found[count - 1] = (optr)malloc(sizeof(optr));
+				*found[count - 1] = p;
 				count++;
 				p = p->next;
 			}
 		}
 		if (count == 0)
 			return ERROR_ELEMENT_NOT_FOUND;
+		*FindAll = count;
+		return OK;
 	}
 	else
 	{
-		while (!p)
+		while (p)
 		{
-			if (strcmp(p->lname, lastName))
+			if (strcmp(p->lname, lastName) == 0)
 			{
 				*found = (optr*)malloc(sizeof(optr));
 				if (*found == NULL)
 					return ERROR_ALLOCATING_MEMORY;
 				*found[0] = p;
+				*FindAll = 1;
 				return OK;
 			}
+			p = p->next;
 		}
 		return ERROR_ELEMENT_NOT_FOUND;
 	}
@@ -147,23 +144,28 @@ char * Catch(int ErrorCode, int* ShuldExit)
 	switch (ErrorCode)
 	{
 	case ERROR_OPENING_FILE:
+		*ShuldExit = ERROR_OPENING_FILE;
 		return "Greska prilikom otvaranja datoteke. Jeste li unijeli tocan path?\n";
 	case ERROR_WRITTING_TO_FILE:
+		*ShuldExit = ERROR_WRITTING_TO_FILE;
 		return "Greska prilikom pisanja u datoteku. Je li datoteka u uporabi?\n";
 	case ERROR_ELEMENT_NOT_FOUND:
+		*ShuldExit = ERROR_ELEMENT_NOT_FOUND;
 		return "Element niza nije pronadjen\n";
 	case ERROR_READING_FILE:
+		*ShuldExit = ERROR_READING_FILE;
 		return "Greska prilikom citanja iz datoteke\n";
 	case ERROR_NULL_ROOT:
-		*ShuldExit = TRUE;
+		*ShuldExit = ERROR_NULL_ROOT;
 		return "Root je NULL\n";
 	case ERROR_EMPTY_LIST:
+		*ShuldExit = ERROR_EMPTY_LIST;
 		return "Lista je prazna\n";
 	case ERROR_ALLOCATING_MEMORY:
-		*ShuldExit = TRUE;
-		return "Malloc faild... fek\n";
+		*ShuldExit = ERROR_ALLOCATING_MEMORY;
+		return "Malloc failed... fek\n";
 	case ERROR_UNESPECTED_NULL_PARAMETER:
-		*ShuldExit = TRUE;
+		*ShuldExit = ERROR_UNESPECTED_NULL_PARAMETER;
 		return "Pogresan parametar";
 	case OK:
 		return "[OK]\n";
--- StrukturePodataka/Vjezba2/Helper.h
+++ StrukturePodataka/Vjezba2/Helper.h
@@ -8,10 +8,10 @@
 //Error Codes
 #define ERROR_OPENING_FILE -404
 #define ERROR_WRITTING_TO_FILE -403
-#define ERROR_ELEMENT_NOT_FOUND -100
+#define ERROR_ELEMENT_NOT_FOUND 100
 #define ERROR_READING_FILE -400
 #define ERROR_NULL_ROOT -1
-#define ERROR_EMPTY_LIST -2
+#define ERROR_EMPTY_LIST 200
 #define ERROR_ALLOCATING_MEMORY -101
 #define ERROR_UNESPECTED_NULL_PARAMETER -3
 #define OK 0
@@ -28,11 +28,13 @@ typedef struct _osoba
 } *optr;
 
 //Methods delcaration
+optr CreateElement(char* n, char* l, int god);
 int AddToBegining(optr root, char* name, char* lname, int god);
 int AddToEnd(optr root, char* name, char* lname, int god);
 int ShowList(optr root);
+int ShowElement(optr element);
 int Delete(optr root, optr elemnt);
-int FindElements(optr root,optr** found ,char* lastName, bool FindAll);
+int FindElements(optr root,optr** found ,char* lastName, int * FindAll);
 char* Catch(int ErrorCode, int* shouldExit);
 //int WriteToFIle(optr root, char* filepath, int append, optr* AdditionalPersons);
 //int ReadFile(optr root, char* path, optr seperateList);
